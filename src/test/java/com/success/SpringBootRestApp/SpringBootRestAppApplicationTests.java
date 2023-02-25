18f001b4cc97257ca71369d21dacbde2c7e24f0a
adminstrator <nguyenthanhcong.dn.cv@gmail.com>
2023-02-25 19:52:35 +0700

    initial version

---
+++ src/test/java/com/success/SpringBootRestApp/SpringBootRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.success.SpringBootRestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
